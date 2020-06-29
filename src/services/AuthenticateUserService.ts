e6fa1b00f7e17a917b29dbbc72f95ceff85f6318
th92rodr <thiago.clrodrigues@gmail.com>
2020-06-29 18:42:05 -0300

    use AppError instead of the default js variable Error

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -4,6 +4,7 @@ import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 
 import authConfig from "../config/auth";
+import AppError from "../errors/AppError";
 
 interface Request {
   email: string;
@@ -20,13 +21,13 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error("Invalid email/password combination.");
+      throw new AppError("Invalid email/password combination.", 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error("Invalid email/password combination.");
+      throw new AppError("Invalid email/password combination.", 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
