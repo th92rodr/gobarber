b51362646b38e610dfc3d7b7b1bc3dff94bd87e3
th92rodr <thiago.clrodrigues@gmail.com>
2020-06-29 17:46:22 -0300

    add patch users/avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import CreateUserService from "../services/CreateUser.service";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import uploadConfig from "../config/upload";
+import UpdateUserAvatarService from "../services/UpdateUserAvatar.service";
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -23,5 +29,27 @@ usersRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        userID: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+);
 
 export default usersRouter;
