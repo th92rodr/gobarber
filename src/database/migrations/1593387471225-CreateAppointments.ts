7bcc884225aa7f059ac657f9c3690837a0e25cb8
th92rodr <thiago.clrodrigues@gmail.com>
2020-06-29 15:10:54 -0300

    update id type in the migrations

--- src/database/migrations/1593387471225-CreateAppointments.ts
+++ src/database/migrations/1593387471225-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1593387471225 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
