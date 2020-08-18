d9dee7935dfdf72497fba07f71d9f4e4c8f4e1f8
th92rodr <thiago.clrodrigues@gmail.com>
2020-08-18 19:41:15 -0300

    create the index routes file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(routes);
+
 app.listen(3333, () => {
-  console.log("Server running on port 3333...");
+  console.log('Server running on port 3333...');
 });
