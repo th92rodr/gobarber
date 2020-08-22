e36ed1e13d9f68eaea37a56d35c05af8ccf87325
th92rodr <thiago.clrodrigues@gmail.com>
2020-08-22 14:10:30 -0300

    create the users routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import usersRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
