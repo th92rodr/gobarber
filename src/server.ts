879e9390709e63050cb630d0ef1d2ce9f596b8d3
th92rodr <thiago.clrodrigues@gmail.com>
2020-08-23 14:30:31 -0300

    add route to serve the static files

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import express from 'express';
 
 import './database';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
