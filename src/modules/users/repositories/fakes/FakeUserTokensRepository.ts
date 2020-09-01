1f0f37b387233b3f61974ccd4dbe50fc88b0890a
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-01 20:51:05 -0300

    create the fake-user-tokens repository

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,30 @@
+import { uuid } from 'uuidv4';
+
+import IUserTokensRepository from '../IUserTokensRepository';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(userId: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id: userId,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    return this.userTokens.find(userToken => userToken.token === token);
+  }
+}
+
+export default FakeUserTokensRepository;
