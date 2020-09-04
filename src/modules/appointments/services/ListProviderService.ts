21ba034a961aa4fd19411ab41621a976e77780a5
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-04 19:27:48 -0300

    create the list-providers service

---
+++ src/modules/appointments/services/ListProviderService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface IRequest {
+  userId: string;
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ userId }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      exceptUserId: userId,
+    });
+
+    return users;
+  }
+}
+
+export default ListProvidersService;
