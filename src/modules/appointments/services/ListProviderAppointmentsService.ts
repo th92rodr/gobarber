627bab42b72484a36d333822aa77fdb3477dc733
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-04 20:24:20 -0300

    create the list-provider-appointments service

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  providerId: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    providerId,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<Appointment[]> {
+    return this.appointmentsRepository.findAllInDayFromProvider({
+      providerId,
+      day,
+      month,
+      year,
+    });
+  }
+}
+
+export default ListProviderAppointmentsService;
