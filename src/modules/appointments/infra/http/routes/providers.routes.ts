8f84d553418eb6393ce1bfe7b756d48a600942c0
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-04 19:28:50 -0300

    create the providers routes file

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+
+const providersRouter = Router();
+
+const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+
+providersRouter.use(ensureAuthenticated);
+
+providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:id/day-availability',
+  providerDayAvailabilityController.index,
+);
+
+export default providersRouter;
