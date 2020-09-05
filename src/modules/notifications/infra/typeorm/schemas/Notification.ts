27f48570de3167f0673a86d048a851fb5b914eac
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-05 14:22:07 -0300

    create the notification schema

---
+++ src/modules/notifications/infra/typeorm/schemas/Notification.ts
@@ -0,0 +1,30 @@
+import {
+  ObjectID,
+  Entity,
+  Column,
+  ObjectIdColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('notifications')
+class Notification {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  content: string;
+
+  @Column('uuid')
+  recipient_id: string;
+
+  @Column({ default: false })
+  read: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default Notification;
