ff5d2cb26a6a6cd613cf8da48a19067153896ae4
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-06 19:39:03 -0300

    fix bug in appointments controller

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { container } from 'tsyringe';
-import { parseISO } from 'date-fns';
 import { Request, Response } from 'express';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -9,14 +8,12 @@ class AppointmentsController {
     const userId = request.user.id;
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       providerId: provider_id,
       userId,
-      date: parsedDate,
+      date,
     });
 
     return response.json(appointment);
